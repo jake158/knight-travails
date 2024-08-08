e075a703f0e8de13eb1472f32eb3cfbf991c90b5
freeram <kulikovpavel2801@gmail.com>
2024-08-08 12:54:21 -0600

    Add boilerplate

---
+++ KnightTravails/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace KnightTravails
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
